f87e6becd59aa9e1cf5704c17cf18aff8fa8a205
root <johndoe@example.com>
2016-12-11 21:21:18 +0100

    syslog fix

--- nginx-ldap-auth-daemon.py
+++ nginx-ldap-auth-daemon.py
@@ -11,9 +11,9 @@ import logging
 from logging.handlers import SysLogHandler
 
 my_logger = logging.getLogger()
-logger.setLevel(logging.INFO)
+my_logger.setLevel(logging.INFO)
 syslog_formatter = logging.Formatter('%(levelname)s :: %(message)s')
-log_handler = SysLogHandler(address='/dev/log', facility=LOG_LOCAL7)
+log_handler = SysLogHandler(address='/dev/log', facility=SysLogHandler.LOG_LOCAL7)
 log_handler.setFormatter(syslog_formatter)
 my_logger.addHandler(log_handler)
 
